997ffd1b2fcb35519b4ff3b5aa06bf6f699083e6
Spans <spansdev@gmail.com>
2022-12-06 12:30:30 +0200

    Fix typo

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 
 //! A wrapper for the Spotify Web API that (hopefully) doesn't suck (too much) (I think).
 //!
-//! A lot of the functionality is largely opinionated for my own use but I'm trying to make the library ergonimic and
+//! A lot of the functionality is largely opinionated for my own use but I'm trying to make the library ergonomic and
 //! efficient to use. So far only the endpoints I care about are implemented, but if you need some endpoints
 //! implemented, feel free to open an issue.
 //!
